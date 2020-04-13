f3f9b0470ecd23188c949ddf454e407afb163b0d
Alejandro Hernandez Guerrero <alexhg11@gmail.com>
2020-04-13 11:00:17 -0500

    CFDI Use catalog

--- src/Catalog/CFDIUse.php
+++ src/Catalog/CFDIUse.php
@@ -23,7 +23,7 @@ abstract class CFDIUse
     const OTHER_MACHINERY_AND_EQUIPMENT = 'I08';
 
     const MEDICAL_DENTAL_AND_HOSPITAL_FEES = 'D01';
-    const DISABILITY_MEDIAL_EXPENSES = 'D02';
+    const DISABILITY_MEDICAL_EXPENSES = 'D02';
     const FUNERAL_EXPENSES = 'D03';
     const DONATIONS = 'D04';
     const MORTGAGE_INTEREST_EFFECTIVELY_PAID = 'D05';
@@ -62,8 +62,151 @@ abstract class CFDIUse
             'legal_entity'      => true,
         ],
 
+        self::CONSTRUCTIONS => [
+            'name' => [
+                'en' => 'Constructions',
+                'es' => 'Construcciones',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => true,
+        ],
+        self::FURNITURE_OFFICE_EQUIPMENT => [
+            'name' => [
+                'en' => 'Furniture and office equipment due to investments',
+                'es' => 'Mobiliario y equipo de oficina por inversiones',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => true,
+        ],
+        self::TRANSPORTATION_EQUIPMENT => [
+            'name' => [
+                'en' => 'Transportation Equipment',
+                'es' => 'Equipo de transporte',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => true,
+        ],
+        self::COMPUTER_EQUIPMENT_AND_ACCESSORIES => [
+            'name' => [
+                'en' => 'Computer equipment and accessories',
+                'es' => 'Equipo de computo y accesorios',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => true,
+        ],
+        self::DIES_MOLDS_AND_TOOLING => [
+            'name' => [
+                'en' => 'Dies, molds and tooling',
+                'es' => 'Dados, troqueles, moldes, matrices y herramental',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => true,
+        ],
+        self::TELEPHONE_COMMUNICATIONS => [
+            'name' => [
+                'en' => 'Telephone communications',
+                'es' => 'Comunicaciones telefónicas',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => true,
+        ],
+        self::SATELLITE_COMMUNICATIONS => [
+            'name' => [
+                'en' => 'Satellite communications',
+                'es' => 'Comunicaciones satelitales',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => true,
+        ],
+        self::OTHER_MACHINERY_AND_EQUIPMENT => [
+            'name' => [
+                'en' => 'Other machinery and equipment',
+                'es' => 'Otra Maquinaria y equipo',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => true,
+        ],
 
-        // TODO: Implement the others
+        self::MEDICAL_DENTAL_AND_HOSPITAL_FEES => [
+            'name' => [
+                'en' => 'Medical, dental and hospital fees',
+                'es' => 'Honorarios médicos, dentales y gastos hospitalarios',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
+        self::DISABILITY_MEDICAL_EXPENSES => [
+            'name' => [
+                'en' => 'Medical disability expenses',
+                'es' => 'Gastos médicos por incapacidad o discapacidad',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
+        self::FUNERAL_EXPENSES => [
+            'name' => [
+                'en' => 'Funeral expenses',
+                'es' => 'Gastos funerales',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
+        self::DONATIONS => [
+            'name' => [
+                'en' => 'Donations',
+                'es' => 'Donativos',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
+        self::MORTGAGE_INTEREST_EFFECTIVELY_PAID => [
+            'name' => [
+                'en' => 'Mortgage interests efectively paid',
+                'es' => 'Intereses reales efectivamente pagados por créditos hipotecarios (casa habitación)',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
+        self::VOLUNTARY_CONTRIBUTIONS_TO_SAR => [
+            'name' => [
+                'en' => 'Voluntary contributions to SAR',
+                'es' => 'Aportaciones voluntarias al SAR',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
+        self::MEDICAL_INSURANCE_PREMIUM => [
+            'name' => [
+                'en' => 'Medical insurance premium',
+                'es' => 'Primas por seguros de gastos médicos',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
+        self::MANDATORY_SCHOOL_TRANSPORTATION_EXPENSE => [
+            'name' => [
+                'en' => 'Mandatory school transportation expense',
+                'es' => 'Gastos de transportación escolar obligatoria',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
+        self::PENSION_AND_SAVINGS_DEPOSITS => [
+            'name' => [
+                'en' => 'Pension and savings deposits',
+                'es' => 'Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones.',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
+        self::EDUCATION_FEES_AND_TUITION => [
+            'name' => [
+                'en' => 'Education fees and tuition',
+                'es' => 'Pagos por servicios educativos (colegiaturas)',
+            ],
+            'natural_person'    => true,
+            'legal_entity'      => false,
+        ],
 
 
         self::PENDING_DEFINITION => [
